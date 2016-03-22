0cd2db03981eead712f6592fa794966252eb389a
Matej Sladek <matejsladek10@gmail.com>
2016-03-22 16:52:02 +0100

    add parameters

--- src/phoxi_camera_node.cpp
+++ src/phoxi_camera_node.cpp
@@ -1,26 +1,20 @@
 #include <ros/ros.h>
 
 #include <dynamic_reconfigure/server.h>
-#include <pho_driver/TutorialsConfig.h>
+#include <phoxi_camera/TutorialsConfig.h>
 #include <sensor_msgs/PointCloud2.h>
 
 //#define PHOXI_PCL_SUPPORT
 
-void callback(pho_driver::TutorialsConfig &config, uint32_t level) {
-    ROS_INFO("Reconfigure Request: %d %f %s %s %d",
-             config.int_param, config.double_param,
-             config.str_param.c_str(),
-             config.bool_param ? "True" : "False",
-             config.size);
-}
-
 #include "PhoXi.h"
 #include <string>
+
 #if defined(_WIN32)
 #include <windows.h>
 #elif defined (__linux__)
 #include <unistd.h>
 #endif
+
 #include <iostream>
 
 #if defined(_WIN32)
@@ -40,84 +34,169 @@ void callback(pho_driver::TutorialsConfig &config, uint32_t level) {
 #include <opencv2/opencv.hpp>
 //#include "Console/PhotoneoConsole.h"
 
+void init_config(pho::api::PPhoXi &Scanner) {
+    std::cout << "cinit" << std::endl;
+    ros::param::set("~size_height", Scanner->Resolution->Height);
+    ros::param::set("~size_width", Scanner->Resolution->Width);
+    ros::param::set("~capturing_size_height", Scanner->CapturingMode->Resolution.Height);
+    ros::param::set("~capturing_size_width", Scanner->CapturingMode->Resolution.Width);
+    ros::param::set("~capturing_scan_multiplier", Scanner->CapturingSettings->ScanMultiplier);
+    ros::param::set("~acquisition_time", Scanner->AcquisitionTime);
+//    ros::param::set("~trigger_mode", Scanner->TriggerMode);
+    //ros::param::set("~timeout", (int)Scanner->Timeout * -1);
+    //ros::param::set("~processing_settings", Scanner->ProcessingSettings->RequiredConfidence);
+//    ros::param::set("~send_point_cloud", Scanner->OutputSettings->SendPointCloud);
+//    ros::param::set("~send_depth_map", Scanner->OutputSettings->SendDepthMap);
+//    ros::param::set("~send_confidence_map", Scanner->OutputSettings->SendConfidenceMap);
+//    ros::param::set("~send_texture", Scanner->OutputSettings->SendTexture);
+}
+
+void callback(pho::api::PPhoXi &Scanner, phoxi_camera::TutorialsConfig &config, uint32_t level) {
+//void callback(phoxi_camera::TutorialsConfig &config, uint32_t level) {
+    printf("%s%d%s", "\033[", 31, "m");
+    std::cout << "level " << level << std::endl;
+    std::bitset<32> x(level);
+    std::cout << "bitmask " << x << std::endl;
+    if (level & (1 << 1)) {
+        Scanner->Resolution->Height = config.size_height;
+    }
+    if (level & (1 << 2)) {
+        Scanner->Resolution->Width = config.size_width;
+    }
+    if (level & (1 << 3)) {
+        Scanner->CapturingMode->Resolution.Height = config.capturing_size_width;
+    }
+    if (level & (1 << 4)) {
+        Scanner->CapturingMode->Resolution.Width = config.capturing_size_width;
+    }
+    if (level & (1 << 5)) {
+        Scanner->CapturingSettings->ScanMultiplier = config.capturing_scan_multiplier;
+    }
+    if (level & (1 << 6)) {
+        Scanner->AcquisitionTime = config.acquisition_time;
+    }
+    std::cout << "koniec callback " << level << std::endl;
+    printf("%s%d%s", "\033[", 0, "m");
+//    if (level & (1 << 9)) {
+//        Scanner->ProcessingSettings->RequiredConfidence = config.processing_settings;
+//    }
+//    if (level & (1 << 8)) {
+//        Scanner->Resolution->width = config.size_width;
+//    }
+//    if (level & (1 << 9)) {
+//        Scanner->Resolution->width = config.size_width;
+//    }
+//    if (level & (1 << 10)) {
+//        Scanner->Resolution->width = config.size_width;
+//    }
+//    if (level & (1 << 11)) {
+//        Scanner->Resolution->width = config.size_width;
+//    }
+//    if (level & (1 << 12)) {
+//        Scanner->Resolution->width = config.size_width;
+//    }
+//    if (level & (1 << 13)) {
+//        Scanner->Resolution->width = config.size_width;
+//    }
+
+}
+
 int main(int argc, char **argv) {
     ROS_INFO("Starting pho_driver ros...");
-    ros::init(argc, argv, "pho_driver");
+    ros::init(argc, argv, "phoxi_camera");
 
     ros::NodeHandle nh;
     ros::Publisher pub;
 
-    dynamic_reconfigure::Server <pho_driver::TutorialsConfig> server;
-    dynamic_reconfigure::Server<pho_driver::TutorialsConfig>::CallbackType f;
-
-    f = boost::bind(&callback, _1, _2);
-    server.setCallback(f);
-
-
-LOCAL_CROSS_SLEEP(5000);
-    pub = nh.advertise<pcl::PointCloud<pcl::PointXYZ>> ("output", 1);
+    dynamic_reconfigure::Server <phoxi_camera::TutorialsConfig> server;
+    dynamic_reconfigure::Server<phoxi_camera::TutorialsConfig>::CallbackType f;
+    ros::AsyncSpinner spinner(4); // Use 4 threads
+    spinner.start();
+//    f = boost::bind(&callback, _1, _2);
+//    server.setCallback(f);
+    //LOCAL_CROSS_SLEEP(5000);
+    pub = nh.advertise < pcl::PointCloud < pcl::PointXYZ >> ("output", 1);
     int k = 0;
-        while(ros::ok()){
-            ROS_INFO("Spin loop");
-            LOCAL_CROSS_SLEEP(1000);
-            pho::api::PhoXiFactory Factory;
-            Factory.StartConsoleOutput("Admin-On");
-            std::vector<pho::api::PhoXiDeviceInformation> DeviceList = Factory.GetDeviceList();
-            for(int k = 0;k < DeviceList.size();k++){
-                if(DeviceList[k].HWIdentification != "file") continue;
-                ROS_INFO("found at least one");
-                pho::api::PPhoXi EvaluationScanner = Factory.Create(DeviceList[k]);
-                EvaluationScanner->Connect();
-                if (EvaluationScanner->isAcquiring()) {
-                    EvaluationScanner->StopAcquisition();
-                }
+    while (ros::ok()) {
+        ROS_INFO("Spin loop");
+//        LOCAL_CROSS_SLEEP(5000);
+//        continue;
+        LOCAL_CROSS_SLEEP(1000);
+        pho::api::PhoXiFactory Factory;
+        Factory.StartConsoleOutput("Admin-On");
+        std::vector <pho::api::PhoXiDeviceInformation> DeviceList = Factory.GetDeviceList();
+        for (int k = 0; k < DeviceList.size(); k++){
+            if (DeviceList[k].HWIdentification[18] != '0' && DeviceList[k].HWIdentification[19] != '1') continue;
+            ROS_INFO("found at least one");
+            pho::api::PPhoXi EvaluationScanner = Factory.Create(DeviceList[k]);
+            EvaluationScanner->Connect();
+            if (EvaluationScanner->isAcquiring()) {
+                EvaluationScanner->StopAcquisition();
+            }
 
-                if (EvaluationScanner->isConnected()) {
-                    EvaluationScanner->TriggerMode = pho::api::PhoXiTriggerMode::Software;
-                    while (EvaluationScanner->GetFrame(0)) {
-                        std::cout << "Cleaning Buffer" << std::endl;
-                    }
-                    EvaluationScanner->StartAcquisition();
-                    if (EvaluationScanner->isAcquiring()) {
-                        for (int i = 0; i < 1; i++) {
-                            if (EvaluationScanner->TriggerImage()) {
-                                pho::api::PFrame MyFrame = EvaluationScanner->GetFrame(pho::api::PhoXiTimeout::Infinity);
-                                EvaluationScanner->AcquisitionTime = 1.0;
-                                if (MyFrame) {
-                                    std::cout << i << std::endl;
-                                    if (!MyFrame->PointCloud.Empty()) std::cout << "PointCloud: " << MyFrame->PointCloud.Size.Width << " x " << MyFrame->PointCloud.Size.Height << " Type: " << MyFrame->PointCloud.GetElementName() << std::endl;
-                                    if (!MyFrame->DepthMap.Empty()) std::cout << "DepthMap: " << MyFrame->DepthMap.Size.Width << " x " << MyFrame->DepthMap.Size.Height << " Type: " << MyFrame->DepthMap.GetElementName() << std::endl;
-                                    if (!MyFrame->Texture.Empty()) std::cout << "Texture: " << MyFrame->Texture.Size.Width << " x " << MyFrame->Texture.Size.Height << " Type: " << MyFrame->Texture.GetElementName() << std::endl;
-                                    if (!MyFrame->ConfidenceMap.Empty()) std::cout << "ConfidenceMap: " << MyFrame->ConfidenceMap.Size.Width << " x " << MyFrame->ConfidenceMap.Size.Height << " Type: " << MyFrame->ConfidenceMap.GetElementName() << std::endl;
-                                    //MyFrame->SaveAsPly("Test Software" + std::to_string(k) + " , " + std::to_string(i) + ".ply");
-                                    //pcl::PointCloud<pcl::PointXYZRGB> MyPCLCloud;
-                                    //MyFrame->ConvertTo(MyPCLCloud);
-                                    //pcl::PCLPointCloud2 MyPCLCloud2;
-                                    //MyFrame->ConvertTo(MyPCLCloud2);
-                                    //pcl::PLYWriter Writer;
-                                    //Writer.writeBinary("Test Software PCL" + std::to_string(k) + " , " + std::to_string(i) + ".ply", MyPCLCloud2);
-                                    pcl::PointCloud<pcl::PointXYZ> cloud;
-                                    int h = MyFrame->PointCloud.Size.Height;
-                                    int w = MyFrame->PointCloud.Size.Width;
-                                    for (int i = 0; i < h; ++i) {
-                                        for (int j = 0; j < w; ++j) {
-                                            auto& point = MyFrame->PointCloud.At(i, j);
-                                            if (point.z > 10 && point.z < 10000)
-                                                cloud.push_back (pcl::PointXYZ (point.x, point.y, point.z));
-                                                // cloud.push_back (pcl::PointXYZ (i, j, i+j));
-                                        }
+            if (EvaluationScanner->isConnected()) {
+                ROS_INFO("Som tam");
+                init_config(EvaluationScanner);
+                f = boost::bind(&callback, boost::ref(EvaluationScanner), _1, _2);
+                server.setCallback(f);
+                LOCAL_CROSS_SLEEP(15000);
+                EvaluationScanner->TriggerMode = pho::api::PhoXiTriggerMode::Software;
+                while (EvaluationScanner->GetFrame(0)) {
+                    std::cout << "Cleaning Buffer" << std::endl;
+                }
+                EvaluationScanner->StartAcquisition();
+                if (EvaluationScanner->isAcquiring()) {
+                    for (int i = 0; i < 1; i++) {
+                        if (EvaluationScanner->TriggerImage()) {
+                            pho::api::PFrame MyFrame = EvaluationScanner->GetFrame(pho::api::PhoXiTimeout::Infinity);
+                            EvaluationScanner->AcquisitionTime = 1.0;
+                            if (MyFrame) {
+                                std::cout << i << std::endl;
+                                if (!MyFrame->PointCloud.Empty())
+                                    std::cout << "PointCloud: " << MyFrame->PointCloud.Size.Width << " x " <<
+                                    MyFrame->PointCloud.Size.Height << " Type: " <<
+                                    MyFrame->PointCloud.GetElementName() << std::endl;
+                                if (!MyFrame->DepthMap.Empty())
+                                    std::cout << "DepthMap: " << MyFrame->DepthMap.Size.Width << " x " <<
+                                    MyFrame->DepthMap.Size.Height << " Type: " << MyFrame->DepthMap.GetElementName() <<
+                                    std::endl;
+                                if (!MyFrame->Texture.Empty())
+                                    std::cout << "Texture: " << MyFrame->Texture.Size.Width << " x " <<
+                                    MyFrame->Texture.Size.Height << " Type: " << MyFrame->Texture.GetElementName() <<
+                                    std::endl;
+                                if (!MyFrame->ConfidenceMap.Empty())
+                                    std::cout << "ConfidenceMap: " << MyFrame->ConfidenceMap.Size.Width << " x " <<
+                                    MyFrame->ConfidenceMap.Size.Height << " Type: " <<
+                                    MyFrame->ConfidenceMap.GetElementName() << std::endl;
+                                //MyFrame->SaveAsPly("Test Software" + std::to_string(k) + " , " + std::to_string(i) + ".ply");
+                                //pcl::PointCloud<pcl::PointXYZRGB> MyPCLCloud;
+                                //MyFrame->ConvertTo(MyPCLCloud);
+                                //pcl::PCLPointCloud2 MyPCLCloud2;
+                                //MyFrame->ConvertTo(MyPCLCloud2);
+                                //pcl::PLYWriter Writer;
+                                //Writer.writeBinary("Test Software PCL" + std::to_string(k) + " , " + std::to_string(i) + ".ply", MyPCLCloud2);
+                                pcl::PointCloud <pcl::PointXYZ> cloud;
+                                int h = MyFrame->PointCloud.Size.Height;
+                                int w = MyFrame->PointCloud.Size.Width;
+                                for (int i = 0; i < h; ++i) {
+                                    for (int j = 0; j < w; ++j) {
+                                        auto &point = MyFrame->PointCloud.At(i, j);
+                                        if (point.z > 10 && point.z < 10000)
+                                            cloud.push_back(pcl::PointXYZ(point.x, point.y, point.z));
+                                        // cloud.push_back (pcl::PointXYZ (i, j, i+j));
                                     }
-                                    ROS_INFO("publishing");
-                                    sensor_msgs::PointCloud2 output;
-                                    pcl::toROSMsg(cloud, output);
-                                    output.header.frame_id = "map";
-                                    pub.publish (output);
                                 }
+                                ROS_INFO("publishing");
+                                sensor_msgs::PointCloud2 output;
+                                pcl::toROSMsg(cloud, output);
+                                output.header.frame_id = "map";
+                                pub.publish(output);
                             }
                         }
                     }
                 }
             }
         }
+    }
     return 0;
 }
