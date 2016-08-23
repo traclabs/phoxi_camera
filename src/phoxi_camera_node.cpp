de01e4bf525a5d87ee6045f22cde312aaa55d0c1
Matej Sladek <matejsladek10@gmail.com>
2016-08-23 17:01:17 +0200

    fix normal_map

--- src/phoxi_camera_node.cpp
+++ src/phoxi_camera_node.cpp
@@ -50,7 +50,7 @@
 
 pho::api::PPhoXi EvaluationScanner;
 pho::api::PhoXiFactory Factory;
-ros::Publisher pub_cloud, pub_normals, pub_confidence_map, pub_texture;
+ros::Publisher pub_cloud, pub_normal_map, pub_confidence_map, pub_texture;
 
 void init_config(pho::api::PPhoXi &Scanner) {
     std::cout << "cinit" << std::endl;
@@ -231,8 +231,8 @@ void publish_frame(pho::api::PFrame MyFrame){
         //MyFrame->ConvertTo(MyPCLCloud2);
         //pcl::PLYWriter Writer;
         //Writer.writeBinary("Test Software PCL" + std::to_string(k) + " , " + std::to_string(i) + ".ply", MyPCLCloud2);
-        pcl::PointCloud <pcl::PointXYZ> cloud, normals;
-        sensor_msgs::Image texture, confidence_map;
+        pcl::PointCloud <pcl::PointXYZ> cloud;
+        sensor_msgs::Image texture, confidence_map, normal_map;
         ros::Time       timeNow         = ros::Time::now();
         std::string     frame           = "camera";
 
@@ -241,6 +241,9 @@ void publish_frame(pho::api::PFrame MyFrame){
 
         confidence_map.header.stamp         = timeNow;
         confidence_map.header.frame_id      = frame;
+
+        normal_map.header.stamp          = timeNow;
+        normal_map.header.frame_id       = frame;
 
         texture.encoding = "32FC1";
         sensor_msgs::fillImage( texture,
@@ -254,8 +257,14 @@ void publish_frame(pho::api::PFrame MyFrame){
                                 sensor_msgs::image_encodings::TYPE_32FC1,
                                 MyFrame->ConfidenceMap.Size.Height, // height
                                 MyFrame->ConfidenceMap.Size.Width, // width
-                                MyFrame->Texture.Size.Width * sizeof(float), // stepSize
+                                MyFrame->ConfidenceMap.Size.Width * sizeof(float), // stepSize
                                 MyFrame->ConfidenceMap.operator[](0));
+        sensor_msgs::fillImage( normal_map,
+                                sensor_msgs::image_encodings::TYPE_32FC3,
+                                MyFrame->NormalMap.Size.Height, // height
+                                MyFrame->NormalMap.Size.Width, // width
+                                MyFrame->NormalMap.Size.Width * sizeof(float) * 3, // stepSize
+                                MyFrame->NormalMap.operator[](0));
         int h = MyFrame->PointCloud.Size.Height;
         int w = MyFrame->PointCloud.Size.Width;
         for (int i = 0; i < h; ++i) {
@@ -264,19 +273,16 @@ void publish_frame(pho::api::PFrame MyFrame){
                 auto &point_normal = MyFrame->NormalMap.At(i, j);
                 if (point.z > 0){
                     cloud.push_back(pcl::PointXYZ(point.x * 0.001f, point.y * 0.001f, point.z * 0.001f));
-                    normals.push_back(pcl::PointXYZ(point_normal.x, point_normal.y, point_normal.z));
                 }
                 // cloud.push_back (pcl::PointXYZ (i, j, i+j));
             }
         }
         ROS_INFO("publishing");
         sensor_msgs::PointCloud2 output_cloud;
-        sensor_msgs::PointCloud2 output_normals;
         pcl::toROSMsg(cloud, output_cloud);
-        pcl::toROSMsg(normals, output_normals);
         output_cloud.header.frame_id = "map";
         pub_cloud.publish(output_cloud);
-        pub_normals.publish(output_normals);
+        pub_normal_map.publish(normal_map);
         pub_confidence_map.publish(confidence_map);
         pub_texture.publish(texture);
     }
@@ -331,7 +337,7 @@ int main(int argc, char **argv) {
     ros::ServiceServer service_get_hardware_identification = nh.advertiseService("get_hardware_indentification", get_hardware_identification);
     ros::ServiceServer service_get_supported_capturing_modes = nh.advertiseService("get_supported_capturing_modes", get_supported_capturing_modes);
     pub_cloud = nh.advertise < pcl::PointCloud < pcl::PointXYZ >> ("pointcloud", 1);
-    pub_normals = nh.advertise < pcl::PointCloud < pcl::PointXYZ >> ("normals", 1);
+    pub_normal_map = nh.advertise < sensor_msgs::Image > ("normal_map", 1);
     pub_confidence_map = nh.advertise < sensor_msgs::Image > ("confidence_map", 1);
     pub_texture = nh.advertise < sensor_msgs::Image > ("texture", 1);
     ROS_INFO("Ready");
